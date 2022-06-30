4084d94402625d7193f535b143a432b0337f7685
Denis Pop <pop.denis1902@gmail.com>
2022-06-30 11:24:13 +0300

    Refactoring and more test cases for the first example

--- Hello World/hello.go
+++ Hello World/hello.go
@@ -1,11 +1,16 @@
 package main
 
-import "fmt"
+const spanish = "Spanish"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
-	return "Hello, " + name;
-}
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World";
+	}
 
-func main() {
-	fmt.Println(Hello("World"))
+	if language == spanish {
+		return spanishHelloPrefix + name;
+	}
+	return englishHelloPrefix + name;
 }
--- Hello World/hello_test.go
+++ Hello World/hello_test.go
@@ -3,10 +3,33 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want);
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper() // when the test fails the line number reported will be in our function call rather than inside our test helper
+				   // comment to see what happens. 
+				   // If commented error line is 12
+				   // If not commented error line is wherever the error happened.
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("Say hello to people", func(t *testing.T){
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+	
+		assertCorrectMessage(t, got, want);
+	})
+	t.Run("Say 'Hello, World' by default is no name is specified", func(t *testing.T){
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want);
+	})
+
+	t.Run("in Spanish", func(t *testing.T){
+		got := Hello("Rita", "Spanish")
+		want := "Hola, Rita";
+
+		assertCorrectMessage(t, got, want);
+	})
 }
